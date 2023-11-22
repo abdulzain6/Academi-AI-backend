6682ec5b87d3e60ef3b694c0d43d54da7cdff293
abdulzain6 <abdulzain6@gmail.com>
2023-11-22 20:21:06 +0500

    ptach

--- api/globals.py
+++ api/globals.py
@@ -68,14 +68,14 @@ def get_model(model_kwargs: dict, stream: bool, is_premium: bool, alt: bool = Fa
     
     if not alt:
         if is_premium:
-            model = deepcopy(global_chat_model.premium_model)
+            model = global_chat_model.premium_model
         else:
-            model = deepcopy(global_chat_model.regular_model)
+            model = ChatOpenAI(model_name="gpt-3.5-turbo")
     else:
         if is_premium:
-            model = deepcopy(global_chat_model_alternative.premium_model)
+            model = global_chat_model_alternative.premium_model
         else:
-            model = deepcopy(global_chat_model_alternative.regular_model)
+            model = global_chat_model_alternative.regular_model
             
     for k, v in args.items():
         with suppress(Exception):
@@ -84,9 +84,9 @@ def get_model(model_kwargs: dict, stream: bool, is_premium: bool, alt: bool = Fa
     fallbacks = []
     for fallback in fallback_chat_models:
         if is_premium:
-            fallback_model = deepcopy(fallback.premium_model)
+            fallback_model = fallback.premium_model
         else:
-            fallback_model = deepcopy(fallback.regular_model)
+            fallback_model = fallback.regular_model
 
         for k, v in args.items():
             with suppress(Exception):
@@ -106,18 +106,18 @@ def get_model_and_fallback(model_kwargs: dict, stream: bool, is_premium: bool, a
     model_kwargs = {**model_kwargs, "streaming" : stream}
     if is_premium:
         if not alt:
-            model = deepcopy(global_chat_model.premium_model)
+            model = global_chat_model.premium_model
         else:
-            model = deepcopy(global_chat_model_alternative.premium_model)
+            model = global_chat_model_alternative.premium_model
             
         fallback_model = fallback_chat_models[-1].premium_model
     else:
         if not alt:
-            model = deepcopy(global_chat_model.regular_model)
+            model = ChatOpenAI(model_name="gpt-3.5-turbo")
         else:
-            model = deepcopy(global_chat_model_alternative.regular_model)
+            model = global_chat_model_alternative.regular_model
             
-        fallback_model = deepcopy(fallback_chat_models[-1].regular_model)
+        fallback_model = fallback_chat_models[-1].regular_model
 
     for k, v in model_kwargs.items():
         with suppress(Exception):
