2838c207f874af055f33c8e321e069917b4c7d1a
Zain <77825794+abdulzain6@users.noreply.github.com>
2023-12-08 15:24:13 +0500

    Update knowledge_manager.py

--- api/lib/knowledge_manager.py
+++ api/lib/knowledge_manager.py
@@ -237,7 +237,7 @@ class KnowledgeManager:
             file_path=filepath,
             api_key=self.unstructured_api_key,
             url=self.unstructured_url,
-            strategy="auto",
+            strategy="fast",
             ocr_languages=[
                 "eng",  # English
                 "spa",  # Spanish
