5107ee77f255d052700be6067b9aa87bb974c07c
Zain <77825794+abdulzain6@users.noreply.github.com>
2023-12-27 15:09:08 +0500

    Update knowledge_manager.py

--- api/lib/knowledge_manager.py
+++ api/lib/knowledge_manager.py
@@ -154,7 +154,7 @@ class KnowledgeManager:
         qdrant_url: str,
         azure_ocr: AzureOCR,
         azure_form_rec_client: DocumentAnalysisClient,
-        chunk_size: int = 1500,
+        chunk_size: int = 750,
         advanced_ocr_page_count: int = 30,
         qdrant_collection_name: str = "academi"
     ) -> None:
