d6bafe5e94a63ecf0f4f7d75af01aad17ee24b72
abdulzain6 <abdulzain6@gmail.com>
2024-06-09 12:55:09 +0500

    Gemini flash

--- api/lib/maths_solver/ocr.py
+++ api/lib/maths_solver/ocr.py
@@ -5,7 +5,7 @@ from langchain.schema import HumanMessage
 
 class ImageOCR:        
     def gemini_ocr(self, image_path: str) -> str:
-        llm = ChatGoogleGenerativeAI(model="gemini-pro-vision", safety_settings = {
+        llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", safety_settings = {
                 HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_NONE,
                 HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
                 HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
